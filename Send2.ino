02f747a5309c3a5e9219532c578c429fcdd190aa
Maxime29 <lemaire1@lacroixrouge-brest.fr>
2017-04-03 09:41:20 +0200

    Add files via upload

---
+++ Send2.ino
@@ -0,0 +1,183 @@
+// Simple RFM12B sender program, with ACK and optional encryption
+// It initializes the RFM12B radio with optional encryption and passes through any valid messages to the serial port
+// felix@lowpowerlab.com
+
+
+#include <RFM12B.h>
+//#include <VirtualWire.h>
+#include <OneWire.h>
+#include <avr/sleep.h>
+
+// You will need to initialize the radio by telling it what ID it has and what network it's on
+// The NodeID takes values from 1-127, 0 is reserved for sending broadcast messages (send to all nodes)
+// The Network ID takes values from 0-255
+// By default the SPI-SS line used is D10 on Atmega328. You can change it by calling .SetCS(pin) where pin can be {8,9,10}
+#define NODEID        2  //network ID used for this unit
+#define NETWORKID    99  //the network ID we are on
+#define GATEWAYID     1  //the node ID we're sending to
+#define ACK_TIME     50  // # of ms to wait for an ack
+#define SERIAL_BAUD  115200
+
+//encryption is OPTIONAL
+//to enable encryption you will need to:
+// - provide a 16-byte encryption KEY (same on all nodes that talk encrypted)
+// - to call .Encrypt(KEY) to start encrypting
+// - to stop encrypting call .Encrypt(NULL)
+uint8_t KEY[] = "ABCDABCDABCDABCD";
+
+int interPacketDelay = 1000; //wait this many ms between sending packets
+char input = 0;
+
+// Need an instance of the Radio Module
+RFM12B radio;
+byte sendSize = 0;
+char payload[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*(){}[]`|<>?+=:;,.";
+bool requestACK = false;
+
+const byte BROCHE_ONEWIRE = 7;
+
+/* Code de retour de la fonction getTemperature() */
+enum DS18B20_RCODES {
+  READ_OK,  // Lecture ok
+  NO_SENSOR_FOUND,  // Pas de capteur
+  INVALID_ADDRESS,  // Adresse reçue invalide
+  INVALID_SENSOR  // Capteur invalide (pas un DS18B20)
+};
+
+/* Création de l'objet OneWire pour manipuler le bus 1-Wire */
+OneWire ds(BROCHE_ONEWIRE);
+
+/**
+   Fonction de lecture de la température via un capteur DS18B20.
+*/
+byte getTemperature(float *temperature, byte reset_search) {
+  byte data[9], addr[8];
+  // data[] : Données lues depuis le scratchpad
+  // addr[] : Adresse du module 1-Wire détecté
+
+  /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
+  if (reset_search) {
+    ds.reset_search();
+  }
+
+  /* Recherche le prochain capteur 1-Wire disponible */
+  if (!ds.search(addr)) {
+    // Pas de capteur
+    return NO_SENSOR_FOUND;
+  }
+
+  /* Vérifie que l'adresse a été correctement reçue */
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    // Adresse invalide
+    return INVALID_ADDRESS;
+  }
+
+  /* Vérifie qu'il s'agit bien d'un DS18B20 */
+  if (addr[0] != 0x28) {
+    // Mauvais type de capteur
+    return INVALID_SENSOR;
+  }
+
+  /* Reset le bus 1-Wire et sélectionne le capteur */
+  ds.reset();
+  ds.select(addr);
+
+  /* Lance une prise de mesure de température et attend la fin de la mesure */
+  ds.write(0x44, 1);
+  delay(800);
+
+  /* Reset le bus 1-Wire, sélectionne le capteur et envoie une demande de lecture du scratchpad */
+  ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);
+
+  /* Lecture du scratchpad */
+  for (byte i = 0; i < 9; i++) {
+    data[i] = ds.read();
+  }
+
+  /* Calcul de la température en degré Celsius */
+  *temperature = ((data[1] << 8) | data[0]) * 0.0625;
+
+  // Pas d'erreur
+  return READ_OK;
+}
+
+void setup()
+{
+  Serial.begin(SERIAL_BAUD);
+  radio.Initialize(NODEID, RF12_433MHZ, NETWORKID);
+  //radio.Encrypt(KEY);
+  radio.Sleep(); //sleep right away to save power
+  Serial.println("Transmitting...\n\n");
+}
+
+void loop()
+{
+  //serial input of [0-9] will change the transmit delay between 100-1000ms
+  /*if (Serial.available() > 0) {
+    input = Serial.read();
+    if (input >= 48 && input <= 57) //[1..9] = {100..900}ms; [0]=1000ms
+    {
+      interPacketDelay = 100 * (input - 48);
+      if (interPacketDelay == 0) interPacketDelay = 1000;
+      Serial.print("\nChanging delay to ");
+      Serial.print(interPacketDelay);
+      Serial.println("ms\n");
+    }
+  }*/
+
+  float temperature;
+    /* Lit la température ambiante à ~1Hz */
+  if (getTemperature(&temperature, true) != READ_OK) {
+    Serial.println(F("Erreur de lecture du capteur"));
+    return;
+  }
+  /* Affiche la température */
+  Serial.print(F("Temperature : "));
+  Serial.print(temperature, 2);
+  Serial.write(176); // Caractère degré
+  Serial.write('C');
+  Serial.println();
+
+  int d1= (int) temperature;
+  char buf[0]; // "-2147483648\0"
+
+  Serial.print("Sending[");
+  Serial.print(2);
+  Serial.print("]:");
+  Serial.print(d1);
+  /*for (byte i = 0; i < sendSize + 1; i++)
+    Serial.print((char)payload[i]);*/
+
+
+  requestACK =0;
+  //requestACK = !(sendSize % 3); //request ACK every 3rd xmission
+
+  radio.Wakeup();
+  radio.Send(GATEWAYID, d1, 2, requestACK);
+  //radio.Send(GATEWAYID, itoa(d1,buf,2), requestACK);
+  if (requestACK)
+  {
+    Serial.print(" - waiting for ACK...");
+    if (waitForAck()) Serial.print("ok!");
+    else Serial.print("nothing...");
+  }
+  radio.Sleep();
+
+  sendSize = (sendSize + 1) % 88;
+  Serial.println();
+  delay(interPacketDelay);
+
+
+
+}
+
+// wait a few milliseconds for proper ACK, return true if received
+static bool waitForAck() {
+  long now = millis();
+  while (millis() - now <= ACK_TIME)
+    if (radio.ACKReceived(GATEWAYID))
+      return true;
+  return false;
+}
